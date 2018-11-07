37d3fdb6640ded3cc124291a226f2e767e818149
Brian Howard <bhoward@depauw.edu>
2018-11-07 15:49:19 -0500

    Added synchronization to Account.

--- Bank/src/main/java/edu/depauw/csc232/Account.java
+++ Bank/src/main/java/edu/depauw/csc232/Account.java
@@ -44,7 +44,7 @@ public class Account {
     * 
     * @return the current balance
     */
-   public Money balance() {
+   public synchronized Money balance() {
       return balance;
    }
 
@@ -53,13 +53,31 @@ public class Account {
     * 
     * @param amount how much money to deposit
     */
-   public void deposit(Money amount) {
+   public synchronized void deposit(Money amount) {
       balance = balance.add(amount);
    }
 
    /**
+    * Attempt to withdraw money from this account. If unable to complete the
+    * action, return false and leave the balance unchanged.
+    * 
+    * @param amount of money to withdraw
+    * @return true if successful
+    */
+   public synchronized boolean withdraw(Money amount) {
+      if (rules.canWithdraw(this, amount)) {
+         balance = balance.subtract(amount);
+         return true;
+      } else {
+         return false;
+      }
+   }
+
+   /**
     * Attempt to transfer money into this account. If unable to complete the
-    * transfer, return false and leave both balances unchanged.
+    * transfer, return false and leave both balances unchanged. Not synchronized to
+    * avoid deadlock, and because any access to the balance is made through
+    * synchronized methods.
     * 
     * @param amount how much money to transfer
     * @param from   which account to take the money from
@@ -75,7 +93,9 @@ public class Account {
 
    /**
     * Attempt to transfer money out of this account. If unable to complete the
-    * transfer, return false and leave both balances unchanged.
+    * transfer, return false and leave both balances unchanged. Not synchronized to
+    * avoid deadlock, and because any access to the balance is made through
+    * synchronized methods.
     * 
     * @param amount how much money to transfer
     * @param to     which account to put the money in
@@ -86,23 +106,9 @@ public class Account {
    }
 
    /**
-    * Attempt to withdraw money from this account. If unable to complete the
-    * action, return false and leave the balance unchanged.
-    * 
-    * @param amount of money to withdraw
-    * @return true if successful
-    */
-   public boolean withdraw(Money amount) {
-      if (rules.canWithdraw(this, amount)) {
-         balance = balance.subtract(amount);
-         return true;
-      } else {
-         return false;
-      }
-   }
-
-   /**
-    * Perform any required end-of-day processing on this account.
+    * Perform any required end-of-day processing on this account. Not synchronized
+    * to avoid deadlock, and because any access to the balance is made through
+    * synchronized methods.
     */
    public void processEndOfDay() {
       rules.processEndOfDay(this);
@@ -111,7 +117,8 @@ public class Account {
    /**
     * Perform any required end-of-month processing on this account. This will be
     * called after {@link edu.depauw.csc232.Account#processEndOfDay()} on the last
-    * day of the month.
+    * day of the month. Not synchronized to avoid deadlock, and because any access
+    * to the balance is made through synchronized methods.
     */
    public void processEndOfMonth() {
       rules.processEndOfMonth(this);
